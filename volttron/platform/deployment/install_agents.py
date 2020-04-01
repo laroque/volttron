21a90105833543ae958019519830801533cebc2c
Ben LaRoque <benjamin.laroque@gmail.com>
2020-04-01 14:02:05 -0700

    distribute

--- volttron/platform/deployment/install_agents.py
+++ volttron/platform/deployment/install_agents.py
@@ -148,6 +148,8 @@ def install_agent_directory(opts, package, agent_config):
     add_files_to_package(opts.package, {'config_file': config_file})
     env = os.environ.copy()
 
+
+    _log().warning(f"BHL install: {opts.package}") #TODO BHL
     if agent_exists:
         cmds = [volttron_control, "upgrade", opts.vip_identity, opts.package]
     else:
